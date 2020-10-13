c9ff0c3fa005d6f56004c0d5d749f3e8d8d766df
Jeroen Breen <>
2020-10-13 12:32:12 +0200

    Update signaling-systems.js

--- src/data/signaling-systems.js
+++ src/data/signaling-systems.js
@@ -128,6 +128,54 @@ let signalingSystems = [
                 }
             }
         ]
+    }, {
+        id: 5,
+        title: 'Routekaart',
+        days: 7,
+        population: 100000,
+        thresholds: [
+            {
+                n: 0,
+                color: {
+                    regular: 'rgb(144,250,5)',
+                    colorblind1: '#333',
+                    colorblind2: '#ffffb2',
+                    variant3: '#f2f0f7'
+                }
+            }, {
+                n: 50,
+                color: {
+                    regular: 'rgb(255,255,0)',
+                    colorblind1: '#66c2a5',
+                    colorblind2: '#fecc5c',
+                    variant3: '#cbc9e2'
+                }
+            }, {
+                n: 150,
+                color: {
+                    regular: 'rgb(250,172,5)',
+                    colorblind1: '#8da0cb',
+                    colorblind2: '#fd8d3c',
+                    variant3: '#9e9ac8'
+                }
+            }, {
+                n: 250,
+                color: {
+                    regular: 'rgb(255,0,0)',
+                    colorblind1: '#fc8d62',
+                    colorblind2: '#e31a1c',
+                    variant3: '#6a51a3'
+                }
+            }, {
+                n: Infinity,
+                color: {
+                    regular: 'rgb(40,40,40)',
+                    colorblind1: 'rgb(40,40,40)',
+                    colorblind2: 'rgb(40,40,40)',
+                    variant3: 'rgb(40,40,40)'
+                }
+            }
+        ]
     }
 ];
 
