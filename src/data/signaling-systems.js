77103e7e04b8515a4b4f4c96cac7cf56c3777117
Jeroen Breen <>
2020-11-02 18:00:32 +0100

    Update signaling-systems.js

--- src/data/signaling-systems.js
+++ src/data/signaling-systems.js
@@ -1,53 +1,5 @@
 let signalingSystems = [
     {
-        id: 1,
-        title: 'WHO',
-        days: 7,
-        population: 100000,
-        thresholds: [
-            {
-                n: 0,
-                color: {
-                    regular: 'rgb(144,250,5)',
-                    colorblind1: '#333',
-                    colorblind2: '#ffffb2',
-                    variant3: '#f2f0f7'
-                }
-            }, {
-                n: 25,
-                color: {
-                    regular: 'rgb(255,255,0)',
-                    colorblind1: '#66c2a5',
-                    colorblind2: '#fecc5c',
-                    variant3: '#cbc9e2'
-                }
-            }, {
-                n: 50,
-                color: {
-                    regular: 'rgb(250,172,5)',
-                    colorblind1: '#8da0cb',
-                    colorblind2: '#fd8d3c',
-                    variant3: '#9e9ac8'
-                }
-            }, {
-                n: 100,
-                color: {
-                    regular: 'rgb(255,0,0)',
-                    colorblind1: '#fc8d62',
-                    colorblind2: '#e31a1c',
-                    variant3: '#6a51a3'
-                }
-            }, {
-                n: Infinity,
-                color: {
-                    regular: 'rgb(40,40,40)',
-                    colorblind1: 'rgb(40,40,40)',
-                    colorblind2: 'rgb(40,40,40)',
-                    variant3: 'rgb(40,40,40)'
-                }
-            }
-        ]
-    }, {
         id: 6,
         title: 'Power of 2',
         days: 7,
@@ -116,6 +68,54 @@ let signalingSystems = [
                     colorblind1: 'rgb(0,0,0)',
                     colorblind2: 'rgb(0,0,0)',
                     variant3: 'rgb(0,0,0)'
+                }
+            }
+        ]
+    }, {
+        id: 1,
+        title: 'WHO',
+        days: 7,
+        population: 100000,
+        thresholds: [
+            {
+                n: 0,
+                color: {
+                    regular: 'rgb(144,250,5)',
+                    colorblind1: '#333',
+                    colorblind2: '#ffffb2',
+                    variant3: '#f2f0f7'
+                }
+            }, {
+                n: 25,
+                color: {
+                    regular: 'rgb(255,255,0)',
+                    colorblind1: '#66c2a5',
+                    colorblind2: '#fecc5c',
+                    variant3: '#cbc9e2'
+                }
+            }, {
+                n: 50,
+                color: {
+                    regular: 'rgb(250,172,5)',
+                    colorblind1: '#8da0cb',
+                    colorblind2: '#fd8d3c',
+                    variant3: '#9e9ac8'
+                }
+            }, {
+                n: 100,
+                color: {
+                    regular: 'rgb(255,0,0)',
+                    colorblind1: '#fc8d62',
+                    colorblind2: '#e31a1c',
+                    variant3: '#6a51a3'
+                }
+            }, {
+                n: Infinity,
+                color: {
+                    regular: 'rgb(40,40,40)',
+                    colorblind1: 'rgb(40,40,40)',
+                    colorblind2: 'rgb(40,40,40)',
+                    variant3: 'rgb(40,40,40)'
                 }
             }
         ]
