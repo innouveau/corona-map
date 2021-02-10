2c87a858c2d0edcde081d39b722a66516b35c2cd
Jeroen Breen <>
2021-02-10 15:27:18 +0100

    Update tweets.R

--- r/src/twitter/tweets/tweets.R
+++ r/src/twitter/tweets/tweets.R
@@ -1,8 +1,6 @@
 get_tweet_1 <- function() {
   text <- "Update #corona #kaart 
   
-Let op: lage cijfers vanwege %s
-
 • %s+ positieve tests
 • %s overleden 
 • besmettingsgraad: %s
@@ -10,7 +8,7 @@ Let op: lage cijfers vanwege %s
 %s zelf kijken in jouw gemeente? https://covid19.innouveau.com/#/?language=nl
 (update omstreeks 15:30)"
   
-  text_merged <- sprintf(text, ICON.snow, get_daily_reported_rounded(), get_daily_deceased(), get_infection_rate(), ICON.hand_pointing_right)
+  text_merged <- sprintf(text, get_daily_reported_rounded(), get_daily_deceased(), get_infection_rate(), ICON.hand_pointing_right)
   Encoding(text_merged) <- "UTF-8"
   return (text_merged)
 }
