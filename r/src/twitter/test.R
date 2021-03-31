bfc6f65f7dda8f5eb33fb543af032e211c662224
Jeroen Breen <jeroen@innouveau.nl>
2021-03-31 16:50:05 +0200

    Update test.R

--- r/src/twitter/test.R
+++ r/src/twitter/test.R
@@ -1,18 +1,12 @@
 library(rtweet)
 
-project_path <- "/Users/jeroenbreen/Documents/work/innouveau/projects/innouveau/corona-map/dev/r"
+project_path <- "/Users/jeroenbreen/Documents/work/innouveau/projects/innouveau/corona-map/code/r"
 
 
 source(paste0(project_path, "/src/twitter/credentials.R"))
 
 
-twitter_token <- create_token(
-  app = twitter_appname,
-  consumer_key = twitter_api_key,
-  consumer_secret = twitter_api_key_secret,
-  access_token = twitter_access_token,
-  access_secret = twitter_access_token_secret)
-
 post_tweet(
-  status = "TEST"
+  status = "TEST",
+  media = paste0(project_path, "/plots/main-municipalities-power_of_2.png")
 )
