87bab8af220cad92c9623ab54b6f9c40301e48ed
Jeroen Breen <jeroen@innouveau.nl>
2022-01-10 13:02:39 +0100

    adjust scale for cumulative

--- frontend/src/tools/canvas.js
+++ frontend/src/tools/canvas.js
@@ -2,6 +2,7 @@ import store from '@/store/store';
 import changeTools from '@/tools/change';
 import { getIncreaseOfType } from "@/tools/calculator";
 import { getShadeOfColor } from "@/tools/color";
+import region from "../components/_pages/main/trends/region";
 
 const addBackground = function(ctx, width, height) {
     ctx.rect(0, 0, width, height);
@@ -13,16 +14,43 @@ const draw = function(ctx, source, regionContainers, settings, view, mapType = '
     ctx.lineWidth = 0.5;
     ctx.strokeStyle = settings.borderStyle ? settings.borderStyle : 'rgba(0,0,0,0.3)';
 
-    for (let regionContainer of regionContainers) {
-        drawRegionContainer(ctx, regionContainer, settings, view, mapType, source);
+    const values = getValues(regionContainers, mapType, view, source);
+    if (mapType === "cumulative") {
+        normalise(values);
+    }
+
+    for (let regionData of values) {
+        drawRegionContainer(ctx, regionData.region, settings, regionData.result.color) ;
     }
 };
 
-const getColorForRegion = function(parent, mapType, view, source) {
+const normalise = (regions) => {
+    const max = Math.max(...regions.map(i => i.result.value), 0);
+    for (const region of regions) {
+        const shade = Math.round(10 * region.result.value / max) / 10;
+        region.result.color = getShadeOfColor('000', Math.min(shade, 1))
+    }
+}
+
+const getValues = (regionContainers, mapType, view, source) => {
+    const result = [];
+    for (const region of regionContainers) {
+        result.push({
+            region: region,
+            result: getValue(region, mapType, view, source)
+        });
+    }
+    return result;
+}
+
+const getValue = function(parent, mapType, view, source) {
     switch (mapType) {
         case 'change':
             let change = parent.getChange(view.offset, changeTools.daysBack);
-            return changeTools.getColorForChange(change);
+            return {
+                value: null,
+                color: changeTools.getColorForChange(change)
+            }
         case 'cumulative':
             const historyLength = store.state.settings.historyLength;
             const start = historyLength - view.offsetStart;
@@ -31,17 +59,23 @@ const getColorForRegion = function(parent, mapType, view, source) {
             const max = (end - start) * 60;
             const increase = getIncreaseOfType(parent, start, end, view.currentSource.key, true);
             const shade = Math.round(10 * increase / max) / 10;
-            return getShadeOfColor('000', Math.min(shade, 1));
+            return {
+                value: increase,
+                color: getShadeOfColor('000', Math.min(shade, 1))
+            }
         default:
-            return parent.getColor(view.offset, source);
+            return {
+                value: null,
+                color: parent.getColor(view.offset, source)
+            }
     }
 }
 
-const drawRegionContainer = function(ctx, parent, settings, view, mapType, source) {
+const drawRegionContainer = function(ctx, parent, settings, color) {
     let currentMap, regionType, pathsOriginSetting, pathsOrigin, children;
     currentMap = store.state.maps.current;
     regionType = parent.regionType;
-    ctx.fillStyle = getColorForRegion(parent, mapType, view, source);
+    ctx.fillStyle = color;
 
 
     if (currentMap.settings.map.discreteRegions && currentMap.settings.map.discreteRegions.indexOf(parent.title) > -1) {
