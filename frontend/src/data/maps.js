ce6d376a901cfe4f6d7cfcfd36a0cdd900933d6c
Jeroen Breen <jeroen@innouveau.nl>
2022-01-11 09:24:55 +0100

    hide old maps

--- frontend/src/data/maps.js
+++ frontend/src/data/maps.js
@@ -106,75 +106,76 @@ const maps = [
                 source: sewageDataUrl
             }
         }
-    }, {
-        id: 2,
-        ready: true,
-        block: false,
-        regionType: 'District',
-        module: 'districts',
-        title: 'Amsterdam',
-        settings: {
-            regionTypes: ['district', 'ggd'],
-            pathOrigins: [
-                {
-                    type: 'ggd',
-                    paths: 'district'
-                }
-            ],
-            generalInfoHasPopulation: false,
-            map: {
-                latitude: 52.32,
-                longitude: 4.86,
-                stretch: 0.62,
-                zoom: 3.5,
-                ratio: 1.1
-            },
-            positiveTestGraph: {
-                zoomFactor: 0.66
-            }
-        },
-        data: {
-            geo: {
-                status: true,
-                source: 'data/maps/amsterdam/amsterdam.json'
-            },
-            positivePcrTests: {
-                status: true,
-                source: 'data/maps/amsterdam/amsterdam-tests.csv',
-                interval: 7,
-                cumulative: false,
-                adapter: {
-                    titleKey: 'Municipality_code',
-                    positiveTestsKey: 'Total_reported.',
-                    administeredTestsKey: 'Total_administered.',
-                    findColumn: function(column) {
-                        return column.indexOf('Total_reported.') > -1;
-                    }
-                }
-            },
-            positiveAntigenTests: {
-                status: false
-            },
-            hospitalisations: {
-                status: false
-            },
-            deceased: {
-                status: false
-            },
-            administeredPcrTests: {
-                status: true,
-                source: 'data/maps/amsterdam/amsterdam-tests.csv'
-            },
-            ageGroups: {
-                status: false,
-                source: ''
-            },
-            sewageMeasurements: {
-                status: false,
-                source: ''
-            }
-        }
     },
+    // {
+    //     id: 2,
+    //     ready: true,
+    //     block: false,
+    //     regionType: 'District',
+    //     module: 'districts',
+    //     title: 'Amsterdam',
+    //     settings: {
+    //         regionTypes: ['district', 'ggd'],
+    //         pathOrigins: [
+    //             {
+    //                 type: 'ggd',
+    //                 paths: 'district'
+    //             }
+    //         ],
+    //         generalInfoHasPopulation: false,
+    //         map: {
+    //             latitude: 52.32,
+    //             longitude: 4.86,
+    //             stretch: 0.62,
+    //             zoom: 3.5,
+    //             ratio: 1.1
+    //         },
+    //         positiveTestGraph: {
+    //             zoomFactor: 0.66
+    //         }
+    //     },
+    //     data: {
+    //         geo: {
+    //             status: true,
+    //             source: 'data/maps/amsterdam/amsterdam.json'
+    //         },
+    //         positivePcrTests: {
+    //             status: true,
+    //             source: 'data/maps/amsterdam/amsterdam-tests.csv',
+    //             interval: 7,
+    //             cumulative: false,
+    //             adapter: {
+    //                 titleKey: 'Municipality_code',
+    //                 positiveTestsKey: 'Total_reported.',
+    //                 administeredTestsKey: 'Total_administered.',
+    //                 findColumn: function(column) {
+    //                     return column.indexOf('Total_reported.') > -1;
+    //                 }
+    //             }
+    //         },
+    //         positiveAntigenTests: {
+    //             status: false
+    //         },
+    //         hospitalisations: {
+    //             status: false
+    //         },
+    //         deceased: {
+    //             status: false
+    //         },
+    //         administeredPcrTests: {
+    //             status: true,
+    //             source: 'data/maps/amsterdam/amsterdam-tests.csv'
+    //         },
+    //         ageGroups: {
+    //             status: false,
+    //             source: ''
+    //         },
+    //         sewageMeasurements: {
+    //             status: false,
+    //             source: ''
+    //         }
+    //     }
+    // },
     {
         id: 4,
         ready: true,
@@ -406,82 +407,83 @@ const maps = [
             }
         }
     },
+    // {
+    //     id: 12,
+    //     ready: true,
+    //     block: false,
+    //     regionType: 'District',
+    //     module: 'cities',
+    //     title: 'Slovakia',
+    //     settings: {
+    //         regionTypes: ['city', 'country'],
+    //         pathOrigins: [
+    //             {
+    //                 type: 'country',
+    //                 paths: 'city'
+    //             }
+    //         ],
+    //         generalInfoHasPopulation: true,
+    //         map: {
+    //             latitude: 48.95,
+    //             longitude: 19.3,
+    //             stretch: 0.65,
+    //             zoom: 0.235,
+    //             ratio: 1.5,
+    //             discreteRegions: []
+    //         },
+    //         positiveTestGraph: {
+    //             zoomFactor: 0.2
+    //         }
+    //     },
+    //     data: {
+    //         geo: {
+    //             status: true,
+    //             source: 'data/maps/slovakia/slovakia.json'
+    //         },
+    //         positivePcrTests: {
+    //             status: true,
+    //             source: 'data/maps/slovakia/positive-pcr-tests.csv',
+    //             interval: 1,
+    //             cumulative: true,
+    //             adapter: standardPcrTestAdapter
+    //         },
+    //         administeredPcrTests: {
+    //             status: false,
+    //             source: ''
+    //         },
+    //         positiveAntigenTests: {
+    //             status: true,
+    //             source: 'data/maps/slovakia/positive-antigen-tests.csv',
+    //             adapter: {
+    //                 titleKey: 'region',
+    //                 getPositiveKeys: function(column) {
+    //                     return column.indexOf('positive') > -1;
+    //                 },
+    //                 getTotalKeyFromDate:function(date) {
+    //                     return date + '_total';
+    //                 },
+    //                 getDateFromKey: function(column) {
+    //                     return column.split('_')[0];
+    //                 }
+    //             }
+    //         },
+    //         hospitalisations: {
+    //             status: false
+    //         },
+    //         deceased: {
+    //             status: false
+    //         },
+    //         ageGroups: {
+    //             status: false,
+    //             source: ''
+    //         },
+    //         sewageMeasurements: {
+    //             status: false,
+    //             source: ''
+    //         }
+    //     }
+    // },
     {
-        id: 12,
-        ready: true,
-        block: false,
-        regionType: 'District',
-        module: 'cities',
-        title: 'Slovakia',
-        settings: {
-            regionTypes: ['city', 'country'],
-            pathOrigins: [
-                {
-                    type: 'country',
-                    paths: 'city'
-                }
-            ],
-            generalInfoHasPopulation: true,
-            map: {
-                latitude: 48.95,
-                longitude: 19.3,
-                stretch: 0.65,
-                zoom: 0.235,
-                ratio: 1.5,
-                discreteRegions: []
-            },
-            positiveTestGraph: {
-                zoomFactor: 0.2
-            }
-        },
-        data: {
-            geo: {
-                status: true,
-                source: 'data/maps/slovakia/slovakia.json'
-            },
-            positivePcrTests: {
-                status: true,
-                source: 'data/maps/slovakia/positive-pcr-tests.csv',
-                interval: 1,
-                cumulative: true,
-                adapter: standardPcrTestAdapter
-            },
-            administeredPcrTests: {
-                status: false,
-                source: ''
-            },
-            positiveAntigenTests: {
-                status: true,
-                source: 'data/maps/slovakia/positive-antigen-tests.csv',
-                adapter: {
-                    titleKey: 'region',
-                    getPositiveKeys: function(column) {
-                        return column.indexOf('positive') > -1;
-                    },
-                    getTotalKeyFromDate:function(date) {
-                        return date + '_total';
-                    },
-                    getDateFromKey: function(column) {
-                        return column.split('_')[0];
-                    }
-                }
-            },
-            hospitalisations: {
-                status: false
-            },
-            deceased: {
-                status: false
-            },
-            ageGroups: {
-                status: false,
-                source: ''
-            },
-            sewageMeasurements: {
-                status: false,
-                source: ''
-            }
-        }
-    }, {
         id: 13,
         ready: true,
         block: false,
