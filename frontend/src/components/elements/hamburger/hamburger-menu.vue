8034e6d0bd06d3a5f6aa76019faef253dcb5f9fb
Jeroen Breen <jeroen@innouveau.nl>
2022-01-18 21:46:05 +0100

    move folder

--- frontend/src/components/elements/hamburger/hamburger-menu.vue
+++ frontend/src/components/elements/hamburger/hamburger-menu.vue
@@ -1,9 +1,9 @@
 <script>
-    import LanguageSwitch from "@/components/elements/language/language-switch";
+    import LanguageSwitch from "./language/language-switch";
 
     export default {
         name: 'hamburger-menu',
-        components: {LanguageSwitch},
+        components: { LanguageSwitch },
         props: {},
         computed: {
             pages() {
