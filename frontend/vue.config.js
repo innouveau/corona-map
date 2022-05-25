48d073e1a4ccf051fd0f7a6ba93d51f38ad95e37
Jeroen Breen <jeroen@innouveau.nl>
2022-05-25 12:05:18 +0200

    new setup of project

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,5 +1,7 @@
-module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-        ? ''
-        : '/'
-}
+const { defineConfig } = require("@vue/cli-service");
+module.exports = defineConfig({
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+      ? ''
+      : '/'
+});
